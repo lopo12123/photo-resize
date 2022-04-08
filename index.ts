e2dc73bae1b31886efba5252ddc121a051104b16
chenmh <chenmh@sucsoft.com>
2022-04-08 16:01:48 +0800

    feat: v0.0.1 fixed output size

---
+++ index.ts
@@ -0,0 +1,30 @@
+import { readdir } from "fs";
+import {resolve} from "path";
+import sharp from "sharp";
+import * as fs from "fs";
+
+const size: [number, number] = [50, 50]
+
+readdir(resolve('./input'), (err, files) => {
+    if(err) console.log(err)
+    else {
+        const pics = files.filter((file) => {
+            return file.endsWith('.png') || file.endsWith('.jpg') || file.endsWith('.jpeg')
+        })
+
+        fs.mkdir('./output', (err) => {
+            if(err) console.log(err)
+            else {
+                pics.forEach((pic) => {
+                    sharp(`./input/${pic}`)
+                        .resize(...size)
+                        .toFile(`./output/${pic}`, (err) => {
+                            if(err) console.log(`fail to resize [${pic}]`)
+                            else console.log(`success to resize [${pic}]`)
+                        })
+                })
+            }
+        })
+    }
+})
+
