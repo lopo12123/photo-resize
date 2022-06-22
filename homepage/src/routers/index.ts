96a23ffb3019956310dd088e2515b8f2cf622d8e
lopo <lopo@zju.edu.cn>
2022-06-22 21:09:59 +0800

    feat: router

--- homepage/src/routers/index.ts
+++ homepage/src/routers/index.ts
@@ -1,14 +1,23 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw, RouterOptions } from "vue-router";
 
-import Home from "@/views/Home.vue";
+import Adaptor from "@/views/Adaptor.vue";
 
 const routes: RouteRecordRaw[] = [
     {
         path: '/',
-        name: 'Index',
-        component: Home
+        name: 'Adaptor',
+        component: Adaptor
     },
-    // {}
+    {
+        path: '/desktop',
+        name: 'Desktop',
+        component: () => import("@/views/Desktop.vue")
+    },
+    {
+        path: '/mobile',
+        name: 'Mobile',
+        component: () => import("@/views/Mobile.vue")
+    },
 ]
 
 const routerOptions: RouterOptions = {
